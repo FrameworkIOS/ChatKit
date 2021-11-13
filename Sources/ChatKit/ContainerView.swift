db285c4ae7b4c55091850954d5f14ddddd794ddd
krishpranav <krisna.pranav@gmail.com>
2021-11-13 06:30:32 +0530

    ChatKit: Containerview

---
+++ Sources/ChatKit/ContainerView.swift
@@ -0,0 +1,33 @@
+//
+//  ContainerView.swift
+//  ChatKit
+//
+//  Created by Krisna Pranav on 13/11/21.
+//
+
+import UIKit
+
+open class ContainerView: UIView {
+    open var sizeHandler: ((CGSize) -> Void)?
+    
+    private var validSize: CGSize = .zero
+    
+    open override var frame: CGRect {
+        didSet {
+            validSize = frame.size
+            sizeHandler?(frame.size)
+        }
+    }
+    
+    open override var bounds: CGRect {
+        didSet {
+            validSize = bounds.size
+            sizeHandler?(bounds.size)
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: .frame)
+        commonInit()
+    }
+}
