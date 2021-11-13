5d95a2c92db28ac8ef577256ac0541f2adc7d7eb
krishpranav <krisna.pranav@gmail.com>
2021-11-13 06:40:16 +0530

    Sample App Using ChatKit

---
+++ Sample/SampleChatKitApp/SampleChatKitApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SampleChatKitApp
+//
+//  Created by Apple on 13/11/21.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
+
