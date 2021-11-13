6c7a8c64e32ab39857cb7668722e7ce3f070f5cb
krishpranav <krisna.pranav@gmail.com>
2021-11-13 06:28:27 +0530

    ChatKit: InputPanel

--- Sources/ChatKit/InputPanel.swift
+++ Sources/ChatKit/InputPanel.swift
@@ -9,13 +9,22 @@ import UIKit
 
 public protocol InputPanelDelegate: AnyObject {
     func inputPanel(_ inputPanel: InputPanel, willChange height: CGFloat, duration: TimeInterval, animationCurve: Int)
-
 }
 
 open class InputPanel: UIView {
+    
     open weak var delegate: InputPanelDelegate?
     
     open func endInputting(animated: Bool) {
         
     }
+    
+    open func adjust(for size: CGSize, keyboardHeight: CGFloat, duration: TimeInterval, animationCurve: Int) {
+        
+    }
+    
+    open func change(to size: CGSize, keyboardHeight: CGFloat, duration: TimeInterval) {
+        
+    }
+    
 }
