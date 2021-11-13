a198617f30af67b842d4ec71a01d5d2f675531d7
krishpranav <krisna.pranav@gmail.com>
2021-11-13 06:02:13 +0530

    ChatKit: ItemCell

--- Sources/ChatKit/ItemCell.swift
+++ Sources/ChatKit/ItemCell.swift
@@ -10,11 +10,29 @@ import UIKit
 public protocol ItemCellDelegate: AnyObject { }
 
 open class ItemCell: UICollectionViewCell {
-    open class var reuseIdentifier: STring { NSStringFromClass(Self.self) }
+    open class var reuseIdentifier: String { NSStringFromClass(Self.self) }
     
     open weak var delegate: ItemCellDelegate?
+
+    open var layout: AnyItemLayout? {
+        didSet {
+            if let size = layout?.size, size != itemView.frame.size {
+                itemView.frame = CGRect(origin: .zero, size: size)
+            }
+        }
+    }
     
     public let itemView = UIView()
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public func commonInit() {
+        itemView.frame = contentView.bounds
+        contentView.addSubview(itemView)
+    }
+
     
 }
