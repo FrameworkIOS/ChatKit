2ce5e4d7dcef9cf679a36686f6d4b99029614821
krishpranav <krisna.pranav@gmail.com>
2021-11-13 06:28:08 +0530

    ChatKit: InputPanel

--- Sources/ChatKit/InputPanel.swift
+++ Sources/ChatKit/InputPanel.swift
@@ -5,4 +5,17 @@
 //  Created by Krisna Pranav on 13/11/21.
 //
 
-import Foundation
+import UIKit
+
+public protocol InputPanelDelegate: AnyObject {
+    func inputPanel(_ inputPanel: InputPanel, willChange height: CGFloat, duration: TimeInterval, animationCurve: Int)
+
+}
+
+open class InputPanel: UIView {
+    open weak var delegate: InputPanelDelegate?
+    
+    open func endInputting(animated: Bool) {
+        
+    }
+}
